eac5bbab1d401a7823112cfb51ff638ce8fd5c85
Vishapasar <77992836+Vishapasar@users.noreply.github.com>
2021-02-10 09:55:03 +0400

    Update map.js
    popup info stage 1

--- mapping early new york v2/timeline/js/map.js
+++ mapping early new york v2/timeline/js/map.js
@@ -1,4 +1,5 @@
-
+var layer_view_flag2 = false;
+$("#studioMenu2").slideUp();
 
 
 /////////////////////////////
@@ -53,11 +54,11 @@ var map = new mapboxgl.Map({
 
         //Before map
         var nav = new mapboxgl.NavigationControl();
-        beforeMap.addControl(nav, "top-left");
+        beforeMap.addControl(nav, "top-right");
 
         //After map
         var nav = new mapboxgl.NavigationControl();
-        afterMap.addControl(nav, "top-left");
+        afterMap.addControl(nav, "top-right");
 
 
 
@@ -164,6 +165,22 @@ afterMap.on("error", function (e) {
 	if (e && e.error !== "Error") console.log(e);
 });
 
+        /*
+        beforeMap.on('click', function () {
+			        if(layer_view_flag2) {
+				        $("#studioMenu2").slideUp();
+						layer_view_flag2 = false;
+					}
+		});
+		
+		afterMap.on('click', function () {
+			        if(layer_view_flag2) {
+				        $("#studioMenu2").slideUp();
+						layer_view_flag2 = false;
+					}
+		});
+		*/
+
 //////////////////////////////////////////////
 //TIME LAYER FILTERING. NOT SURE HOW WORKS.
 //////////////////////////////////////////////
@@ -260,7 +277,14 @@ function changeDate(unixDate) {
 	beforeMap.setFilter("c7_dates-ajsksu-left", dateFilter);
 	afterMap.setFilter("c7_dates-ajsksu-right", dateFilter);
 
-
+	var layer_features = afterMap.queryRenderedFeatures({ layers: ['c7_dates-ajsksu-right'] });
+	//console.log(layer_features[0].properties.DATE2);
+	//console.log(layer_features[0].properties);
+	//buildPopUpInfo(layer_features[0].properties);
+	                if(layer_view_flag2) {
+						buildPopUpInfo(layer_features[0].properties);
+					}
+	
 }//end function changeDate
 
 
@@ -657,6 +681,7 @@ function addBeforeLayers(yr, date) {
 
 
 			//POPUP CONTENTS
+			/*
 			new mapboxgl.Popup()
 				.setLngLat(coordinates)
 
@@ -862,7 +887,18 @@ function addBeforeLayers(yr, date) {
 
 				)
 				.addTo(beforeMap);
+				*/
+				
+				    if(layer_view_flag2) {
+				        $("#studioMenu2").slideUp();
+						layer_view_flag2 = false;
+					} else {
+						buildPopUpInfo(e.features[0].properties);
+					    $("#studioMenu2").slideDown();
+						layer_view_flag2 = true;
+					}
 		});
+		
 
 		// CHANGE TO CURSOR WHEN HOVERING
 		beforeMap.on('mouseenter', 'c7_dates-ajsksu-left', function () {
@@ -963,8 +999,11 @@ function addAfterLayers(yr, date) {
 				coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
 			}
 
+            //console.log(e.features[0].properties.DATE2);
+            //console.log(e.features[0].properties);
 
 			//POPUP CONTENTS
+	        /*
 			new mapboxgl.Popup()
 				.setLngLat(coordinates)
 
@@ -1170,8 +1209,22 @@ function addAfterLayers(yr, date) {
 
 				)
 				.addTo(afterMap);
+				*/
+				
+				    if(layer_view_flag2) {
+				        $("#studioMenu2").slideUp();
+						layer_view_flag2 = false;
+					} else {
+						buildPopUpInfo(e.features[0].properties);
+					    $("#studioMenu2").slideDown();
+						layer_view_flag2 = true;
+					}
 		});
-
+		          
+		           
+		            
+                    
+					
 		// CHANGE TO CURSOR WHEN HOVERING
 		afterMap.on('mouseenter', 'c7_dates-ajsksu-right', function () {
 			afterMap.getCanvas().style.cursor = 'pointer';
@@ -1182,7 +1235,212 @@ function addAfterLayers(yr, date) {
 			afterMap.getCanvas().style.cursor = '';
 		})
 	//});
+}
 
+
+
+
+function buildPopUpInfo(props) {
+				var popup_html =
+
+					///////
+					//TITLE
+					///////
+					"<b><h2>Taxlot: <a href='https://nahc-mapping.org/mappingNY/encyclopedia/taxlot/c7' target='_blank'>C7</a></h2></b>" +
+// CAN'T GET THE TAXLOT LINK TO WORK: <a href='https://nahc-mapping.org/mappingNY/encyclopedia/taxlot/c7'>
+					////////////////
+					//PROPERTY TYPE
+					////////////////
+					"<b>House</b>" +
+
+
+					//LINE
+					"<hr>" +
+
+
+					/////////////
+					//DATE RANGE
+					////////////
+
+					//FROM
+					//example: June 3, 1643
+					"<b> FROM: </b>" +
+					props.DATE1 +
+
+					//TO
+					//example: January 19, 1659
+					"<br>" +
+					"<b> TO: </b>" +
+					props.DATE2 +
+
+
+
+					/////////////////////////////////////////////////////////////////////////////////////////////
+					//UNKNOWN (DISPLAY TITLE AND EXPLANATION WHERE UNKNOWN OR NOTHING, %nbsp)
+					//example 1: <br><br><b>TAXLOT EVENTS UNKNOWN</b><br>Needs research beyond sources used.
+					//example 2: &nbsp;
+					//////////////////////////////////////////////////////////////////////////////////////////////
+					props.Unknown +
+
+
+
+					//LINE
+					"<hr>" +
+
+
+
+
+					//KEEP THIS AS ALTERNATIVE WAY OF LINKING:
+					//'<a href="' + props.tax_lots_3 + '" target="_blank">' + props.tax_lots_3 + '</a>'
+
+
+
+					//////////////////////////////////////////////////
+					//NEXT
+					//example 1: <b>OWNERSHIP:</b><br>
+					//example 2: <b>NEXT KNOWN OWNERSHIP:</b><br>
+					//////////////////////////////////////////////////
+					props.Next +
+
+
+
+
+					///////////////////////
+					//OWNERS EXAMPLES:
+					//////////////////////
+
+					//NOTE: Not sure if NULLs are a problem, check in the future.
+
+					//TAXLOT EVENT PARTY ROLE 1
+					//TO_PAR1 / TO_PAR2 / FROM_PAR1 / FROM PAR2
+					//example 1: /nahc/encyclopedia/node/1537 hreflang="en" target="_blank">Joint Owner</a>
+					//example 2: NULL
+
+					//FROM PARTY 1 (ANCESTOR)
+					//TO_1 / T0_2 / FROM_1 / FROM_2
+					//example 1: /nahc/encyclopedia/node/1536 hreflang="en" target="_blank">Signatory</a>
+					//example 2: NULL
+
+					//TAXLOT ENTITY DESCRIPTIONS 2
+					//TO_ENT1 / TO_ENT2 / FROM_ENT1 / FROM_ENT2
+					//example 1: /nahc/encyclopedia/node/1535 hreflang="en" target="_blank">Corporation</a>
+					//example 2: NULL
+
+
+
+
+					//////////////////////////////////////////
+					//TO OWNERS
+					//examples: See Above (OWNER EXAMPLES)
+					//////////////////////////////////////////
+
+
+
+					//OWNER 1
+					'<a href=https://nahc-mapping.org/mappingNY' + props.TO_PAR1 + ": " +
+					"<br>" +
+					'<a href=https://nahc-mapping.org/mappingNY' + props.TO_1 +
+					" (" + '<a href=https://nahc-mapping.org/mappingNY' + props.TO_ENT1 + ")" +
+					"<br>" +
+					"<br>" +
+
+					//OWNER 2
+					'<a href=https://nahc-mapping.org/mappingNY' + props.TO_PAR2 + ": " +
+					"<br>" +
+					'<a href=https://nahc-mapping.org/mappingNY' + props.TO_2 +
+					" (" + '<a href=https://nahc-mapping.org/mappingNY' + props.TO_ENT2 + ")" +
+
+					"<br>" +
+					"<br>" +
+
+
+
+					//////////////////
+					//TAXLOT EVENT
+					//////////////////
+
+					//TAXLOT EVENT TITLE
+					//example 1: <b>TAXLOT EVENT:</b>
+					//example 2: <b>NEXT TAXLOT EVENT:</b>
+					props.Tax_Event +
+
+					"<br>" +
+
+
+					//TAXLOT EVENT TYPE
+					//example: /nahc/encyclopedia/node/1528 hreflang="en" target="_blank">Land Grant or Patent</a>
+					'<a href=https://nahc-mapping.org/mappingNY' + props.EVENT1 +
+					"<hr>" +
+
+
+					//////////////////////////////
+					//FROM OWNERS
+					//examples: see above, OWNER EXAMPLES
+					//////////////////////////////
+
+
+					//FROM TITLE
+					//example 1: <b>FROM:</b>
+					//example 2: <b>PREVIOUS KNOWN FROM:</b>
+					props.Previous +
+
+					"<br>" +
+
+					//FROM 1
+
+					//TAXLOT EVENT PARTY ROLE 1
+					'<a href=https://nahc-mapping.org/mappingNY' + props.FROM_PAR1 + ": " +
+					"<br>" +
+					//FROM PARTY 1 (ANCESTOR)
+					'<a href=https://nahc-mapping.org/mappingNY' + props.FROM_1 +
+					//TAXLOT ENTITY DESCRIPTIONS 2
+					" (" + '<a href=https://nahc-mapping.org/mappingNY' + props.FROM_ENT1 + ")" +
+					"<br>" +
+					"<br>" +
+
+
+					//FROM 2
+
+					//TAXLOT EVENT PARTY ROLE 1
+					'<a href=https://nahc-mapping.org/mappingNY' + props.FROM_PAR2 + ": " +
+					"<br>" +
+					//FROM PARTY 2 (ANCESTOR)
+					'<a href=https://nahc-mapping.org/mappingNY' + props.FROM_2 +
+					//TAXLOT ENTITY DESCRIPTIONS 2
+					" (" + '<a href=https://nahc-mapping.org/mappingNY' + props.FROM_ENT2 + ")" +
+
+
+
+
+					///////////////////////////
+					//PREVIOUS TAXLOT EVENT (SHOWS UP IF TAXLOT EVENTS UNKNOWN, OTHERWISE BLANK, &nbsp;)
+					//////////////////////////
+
+					//TITLE: "PREVIOUS TAXLOT EVENT"
+					//example 1: <br><br><b>PREVIOUS TAXLOT EVENT:</b><br>
+					//example 2: &nbsp;
+					props.Event +
+
+					//PREVIOUS TAXLOT EVENT
+					//example 1: /nahc/encyclopedia/node/1528 hreflang="en" target="_blank">Land Grant or Patent</a>
+					//example 2: &nbsp;
+					'<a href=https://nahc-mapping.org/mappingNY' + props.Prev_Event +
+
+
+
+
+
+
+					//LINK TO ALL TAXLOT EVENTS: "SEE ALL TAXLOT EVENTS"
+					"<br>" +
+					"<hr>" +
+					'<b> <h3><a href="https://nahc-mapping.org/mappingNY/encyclopedia/taxlot-events" target="_blank">SEE ALL TAXLOT EVENTS</a></h3></b>'
+
+//NEED TO MAKE THIS OPEN IN SEPARATE TAB!!
+;
+
+//console.log(popup_html);
+$("#studioMenu2").html(popup_html);
 
 }
 
